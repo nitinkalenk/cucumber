5bc6f3b1b905e1fba6d0d3a744263d0416a89a71
nkale <nitinkale.nk@gmail.com>
2023-02-13 00:16:55 +0530

    removed testng test runner

--- src/test/java/org/bdd/sample/runner/TestNgTestRunner.java
+++
@@ -1,9 +0,0 @@
-package org.bdd.sample.runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src\\test\\java\\org\\bdd\\sample\\features", glue = "org.bdd.sample.stepdefinitions")
-public class TestNgTestRunner extends AbstractTestNGCucumberTests {
-
-}
