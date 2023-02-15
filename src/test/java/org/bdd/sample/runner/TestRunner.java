24f1d45ca3e890a11df6ab1f0ac2028590bf1447
nkale <nitinkale.nk@gmail.com>
2023-02-15 12:13:25 +0530

    usage of plugins to generate report

--- src/test/java/org/bdd/sample/runner/TestRunner.java
+++ src/test/java/org/bdd/sample/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src\\test\\java\\org\\bdd\\sample\\features", glue = "org.bdd.sample.stepdefinitions",
-    stepNotifications = true)
+    stepNotifications = true, plugin = {"pretty", "html:target/report.html", "json:target/report.json", "junit:target/junit.xml"})
 public class TestRunner {
 
 }
